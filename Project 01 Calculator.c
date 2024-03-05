e9a2d5ffb2382e23f07b01defefa7d93d9ba000b
RH Rimon <83422826+RH-Rimon@users.noreply.github.com>
2024-03-06 00:18:22 +0600

    Add files via upload
    Not Completed Yet

---
+++ Project 01 Calculator.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    printf("\n\n\n-----------------------------------CALCULATOR----------------------------------\n \n");
+    printf(" \t \t \t \tOperation Menu\n\n");
+    printf("1. Sum  \t 2. Subtraction \t 3. Multiplication \t 4. Division\n");
+    printf("5. log    \t 6. log10  \t\t 7. Exponential(exp) \t 8. Power\n");
+    printf("9. Sine    \t 10. Cosine  \t\t 11. Tangent \n\n");
+    printf(" \t \t \t \tOther Operations \n\n");
+    printf("12. Decimal to Binary \t\t 13. Decimal to Octal\n");
+    printf("14. Decimal to Hexa-Decimal \t 15. Binary to Decimal\n");
+    printf("16. Binary to Octal \t\t 17. Binary to Hexa-Decimal\n\n\n");
+
+    int choice;
+    printf("Enter Your Choice: ");
+    scanf("%d",&choice);
+
+
+
+
+
+
+    return 0;
+}
